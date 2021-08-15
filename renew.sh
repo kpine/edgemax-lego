042bb3e91587b19f09bccf57194945efcde7ffd3
Keith Pine <keith.pine@gmail.com>
2021-08-14 23:27:23 -0700

    Allow lego.cfg to be optional

--- renew.sh
+++ renew.sh
@@ -2,12 +2,7 @@
 
 LEGO_HOME=/config/scripts/lego
 
-if [ ! -f "$LEGO_HOME/lego.cfg" ]; then
-  echo "Settings file does not exist."
-  exit 1
-fi
-
-. "$LEGO_HOME/lego.cfg"
+[ -f "$LEGO_HOME/lego.cfg" ] && . "$LEGO_HOME/lego.cfg"
 
 if [ -z "$LETSENCRYPT_EMAIL" ] || [ -z "$DOMAINS" ] || [ -z "$DNS_PROVIDER" ]; then
   echo "A required enviroment variable is missing (LETSENCRYPT_EMAIL, DOMAINS, DNS_PROVIDER)"
